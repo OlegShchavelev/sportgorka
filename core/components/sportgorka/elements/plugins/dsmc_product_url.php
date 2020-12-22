1a36ef06f84c16e80370b1d01277136a59cc951a
oleg <admin@detali.digital>
2020-12-22 15:01:59 +0300

    Добавлено: Авторматическое формирование URL товара

---
+++ core/components/sportgorka/elements/plugins/dsmc_product_url.php
@@ -0,0 +1,28 @@
+<?php
+switch ($modx->event->name) {
+
+    // Documents
+    case 'OnBeforeDocFormSave':
+        if ($resource->get('class_key') == 'msProduct') {
+
+            $uri = $modx->getOption('ms2_product_as_uri');
+            $pre_uri = preg_replace('/\/.+/', '' , $uri);
+            preg_match_all('~{([^{}]*)}~' , $uri , $alias);
+            $mimeType = $modx->getObject('modContentType' , array('mime_type' => 'text/html'));
+            $suffix = $mimeType->get('file_extensions');
+
+            $url = $pre_uri . '/' . $resource->get($alias[1][0]) . $suffix;
+
+            $resource->set('uri' , $url);
+            $resource->set('uri_override' , true);
+
+            if($modx->getOption('ms2_product_as_uri_debug') == 1) {
+                $modx->log(modX::LOG_LEVEL_ERROR, 'URI изменен на ' . $url);
+            }
+
+            $resource->save();
+        }
+
+        break;
+}
+
